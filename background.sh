1904d7e05bcf22948a94a21cef9dcfe92f569be3
random-toto@localhost <random-toto>
2015-09-15 10:29:04 +0200

    ajout du script bash

---
+++ background.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+while [ 1 ];
+do 
+    sleep 600
+    /usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///usr/local/share/background/theWindRisesVideoCastImag.jpg
+    sleep 600
+    /usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///usr/local/share/background/leVentSeLeveTrain.jpg
+    sleep 600
+    /usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///usr/local/share/background/voyage-de-chihiro-2-1024-640.jpg
+    sleep 600
+    /usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///usr/local/share/background/my-neighbor-totoro.jpg
+    sleep 600
+    /usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///usr/local/share/background/leVentSeLeve.jpg
+    sleep 600
+    /usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///usr/local/share/background/b_1_q_0_p_1.jpg
+    sleep 600
+    /usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///usr/local/share/background/marnieMaison.jpg
+    sleep 600
+    /usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///usr/local/share/background/marnieLac.png
+    sleep 600
+    /usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///usr/local/share/background/marnieHome.jpg
+    sleep 600
+    /usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///usr/local/share/background/marnieBois.jpg
+    sleep 600 
+    /usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///usr/local/share/background/princessMononokeSpirit.jpg
+    sleep 600 
+    /usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///usr/local/share/background/princessMononokeSpiritSmile.png
+    sleep 600 
+    /usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///usr/local/share/background/princessMononokeWolf.jpg
+done
